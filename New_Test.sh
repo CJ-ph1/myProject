be0c7b23dec938b42412be4c614617afe7543b49
CJ <cjaymendezw@gmail.com>
2025-01-28 15:51:34 +0800

    Already Committed

---
+++ New_Test.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+while true; do
+	read -p "Enter Username Here: " input
+	storage=("$input")
+	
+	if [ "$input" = "Exit" ]; then
+		break
+	fi
+	while true; do
+		read -p "Verify Your Name or Exit: " verify
+		if [ "$verify" = "${storage[@]}" ]; then
+			echo "Hello $verify"
+			break
+		elif [ "$verify" = "Exit" ]; then
+			break
+		else	
+			echo "Error who are you?"
+			echo "Halaa nanditoo parin siyaa ohh"
+
+		fi
+	done
+done
